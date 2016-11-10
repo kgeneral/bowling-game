a995f592f4868e4abdef769981664dacd551a733
Bruce DaeYeong Kim <dykim1983@gmail.com>
2016-11-10 15:12:17 +0900

    roll a ball for a game

--- src/test/java/BowlingTest.java
+++ src/test/java/BowlingTest.java
@@ -1,8 +1,14 @@
 import org.junit.Test;
 
 public class BowlingTest {
+
     @Test
     public void nothing() {
-
+    }
+    
+    @Test
+    public void canRoll() {
+        Game g = new Game();
+        g.roll(0);
     }
 }
