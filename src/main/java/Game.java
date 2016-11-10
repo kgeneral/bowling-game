0293ad22c2bf9c224c52bb5390ade6a01248827b
Bruce DaeYeong Kim <dykim1983@gmail.com>
2016-11-10 15:39:48 +0900

    marked todo

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,6 +2,7 @@
 public class Game {
 
     private int score = 0;
+    // TODO : 잘못된 책임 할당(misplaced responsibility)이 디자인 원칙, 잘못된 디자인 냄새이다.
     public void roll(int pins) {
         score += pins;
     }
