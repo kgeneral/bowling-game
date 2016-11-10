2f4a0ff4fabd614fa5131f302f2c7bdc3838af6e
Bruce DaeYeong Kim <dykim1983@gmail.com>
2016-11-10 15:48:49 +0900

    save each roll and do the sum of it, for proper responsibilities

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,13 +1,14 @@
+import java.util.Arrays;
 
 public class Game {
 
-    private int score = 0;
-    // TODO : 잘못된 책임 할당(misplaced responsibility)이 디자인 원칙, 잘못된 디자인 냄새이다.
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
     public void roll(int pins) {
-        score += pins;
+        rolls[currentRoll++] = pins;
     }
 
     public int score() {
-        return score;
+        return Arrays.stream(rolls).sum();
     }
 }
