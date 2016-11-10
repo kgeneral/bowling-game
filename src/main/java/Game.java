9c354096f57d612e29da8af3561426983bc85a82
Bruce DaeYeong Kim <dykim1983@gmail.com>
2016-11-10 16:23:07 +0900

    add strike, extract conditional scoring

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -11,12 +11,25 @@ public class Game {
         int score = 0;
         int firstFrame = 0;
         for(int frame = 0; frame < FRAMES; frame++) {
-            score += rolls[firstFrame] + rolls[firstFrame + 1];
-            if(rolls[firstFrame] + rolls[firstFrame + 1] == 10)
-                score += rolls[firstFrame + 2];
-
-            firstFrame += 2;
+            if(isStrike(firstFrame)) {
+                score += 10 + rolls[firstFrame + 1] + rolls[firstFrame + 2];
+                firstFrame += 1;
+            } else if(isSpare(firstFrame)) {
+                score += 10 + rolls[firstFrame + 2];
+                firstFrame += 2;
+            } else {
+                score += rolls[firstFrame] + rolls[firstFrame + 1];
+                firstFrame += 2;
+            }
         }
         return score;
+    }
+
+    private boolean isSpare(int frame) {
+        return rolls[frame] + rolls[frame + 1] == 10;
+    }
+
+    private boolean isStrike(int frame) {
+        return rolls[frame] == 10;
     }
 }
